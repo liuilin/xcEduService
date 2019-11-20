fcd3cfe25b587013f4c21a074579b04a23833128
iMugen <liu1142865396@gmail.com>
2019-11-20 15:02:05 +0800

    修改：环境搭建，初版定稿，controller及dao层测试均无问题

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -10,8 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
-@ComponentScan("com.xuecheng.api")//扫描接口
-@ComponentScan("com.xuecheng.manage_cms")//扫描本项目下的实体类
+@ComponentScan(basePackages = {"com.xuecheng.api"})//扫描接口
+@ComponentScan(basePackages = {"com.xuecheng.manage_cms"})//扫描本项目下的实体类
 public class ManageCmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageCmsApplication.class, args);
