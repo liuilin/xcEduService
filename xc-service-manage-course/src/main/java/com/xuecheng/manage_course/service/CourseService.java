66c7049367bbee84be5b6dc8dece2be37a19c797
iMugen <liu1142865396@gmail.com>
2020-04-26 17:32:00 +0800

    add:添加课程计划功能完善

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/service/CourseService.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/service/CourseService.java
@@ -1,16 +1,18 @@
 package com.xuecheng.manage_course.service;
 
+import com.xuecheng.framework.domain.course.CourseBase;
 import com.xuecheng.framework.domain.course.Teachplan;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
 import com.xuecheng.framework.exception.CustomException;
 import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.manage_course.dao.CourseBaseRepository;
 import com.xuecheng.manage_course.dao.TeachPlanRepository;
 import com.xuecheng.manage_course.dao.TeachplanMapper;
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
@@ -28,35 +30,72 @@ public class CourseService {
     private TeachplanMapper teachplanMapper;
     @Autowired
     private TeachPlanRepository teachPlanRepository;
+    @Autowired
+    private CourseBaseRepository courseBaseRepository;
 
     public TeachplanNode findTeachplanList(String courseId) {
         return teachplanMapper.findTeachPlanById(courseId);
     }
 
+    public void print(String s) {
+        System.out.print(s);
+    }
+
+    @Transactional
     public ResponseResult addTeachPlan(Teachplan teachplan) {
         if (teachplan == null || StringUtils.isEmpty(teachplan.getCourseid()) || StringUtils.isEmpty(teachplan.getPname())) {
             throw new CustomException(CommonCode.INVALID_PARAMETER);
         }
-        String parentid = teachplan.getParentid();
         String courseid = teachplan.getCourseid();
+        //页面传入的父id，可能没有
+        String parentid = teachplan.getParentid();
         if (StringUtils.isEmpty(parentid)) {
             parentid = this.getTeachPlanRootNode(courseid);
         }
+        Optional<Teachplan> teachplanOptional = teachPlanRepository.findById(parentid);
+        Teachplan parentNode = teachplanOptional.get();
+        //父节点级别
+        String grade = parentNode.getGrade();
         //保存课程
-        Teachplan teachplan1 = new Teachplan();
-        BeanUtils.copyProperties(teachplan,teachplan1);
-        teachplan1.setParentid(parentid);
-        teachplan1.setCourseid(courseid);
-        teachPlanRepository.save(teachplan1);
+        Teachplan teachplanNew = new Teachplan();
+        BeanUtils.copyProperties(teachplan, teachplanNew);
+        teachplanNew.setParentid(parentid);
+        teachplanNew.setCourseid(courseid);
+        //grade级别，根据父节点的级别来设置
+        if (grade.equals("1")) {
+            teachplanNew.setGrade("2");
+        } else {
+            teachplanNew.setGrade("3");
+        }
+        teachPlanRepository.save(teachplanNew);
         return new ResponseResult(CommonCode.SUCCESS);
     }
 
+    /**
+     * 获取根节点，如果没有则添加根节点
+     *
+     * @param courseid
+     * @return
+     */
     private String getTeachPlanRootNode(String courseid) {
-        List<Teachplan> teachplans = teachPlanRepository.findAllByCourseidAndParentid(courseid, "0");
-        Optional.ofNullable(teachplans).orElseThrow(() -> new ServiceException("不存在或是已被删除"));
-        if (CollectionUtils.isEmpty(teachplans)) {
+        Optional<CourseBase> optional = courseBaseRepository.findById(courseid);
+        if (!optional.isPresent()) {
             return null;
         }
-        return teachplans.get(0).getParentid();
+        CourseBase courseBase = optional.get();
+        List<Teachplan> teachplans = teachPlanRepository.findAllByCourseidAndParentid(courseid, "0");
+//        Optional.ofNullable(teachplans).orElseThrow(() -> new ServiceException("不存在或是已被删除"));
+        if (CollectionUtils.isEmpty(teachplans)) {
+            //查询不到根节点，自动添加根节点
+            Teachplan teachplan = new Teachplan();
+            teachplan.setPname(courseBase.getName());
+            teachplan.setParentid("0");
+            teachplan.setGrade("1");
+            teachplan.setCourseid(courseid);
+            teachPlanRepository.save(teachplan);
+            return teachplan.getId();
+        }
+        //返回根节点id
+        return teachplans.get(0).getId();
     }
 }
