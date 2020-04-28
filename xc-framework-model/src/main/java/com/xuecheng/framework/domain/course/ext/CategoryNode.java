3b24ec4ab93cb3529bc57ecaeac34c501cb926d2
iMugen <liu1142865396@gmail.com>
2020-04-28 09:57:16 +0800

    add:课程分类查询

--- xc-framework-model/src/main/java/com/xuecheng/framework/domain/course/ext/CategoryNode.java
+++ xc-framework-model/src/main/java/com/xuecheng/framework/domain/course/ext/CategoryNode.java
@@ -1,12 +1,16 @@
 package com.xuecheng.framework.domain.course.ext;
 
 import com.xuecheng.framework.domain.course.Category;
+import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
 /**
  * @author Daniel Liu 2020/4/27 17:07
  */
+@Data
+@ToString
 public class CategoryNode extends Category {
     List<CategoryNode> children;
 }
--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/CategoryMapper.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/CategoryMapper.java
@@ -1,10 +1,12 @@
 package com.xuecheng.manage_course.dao;
 
 import com.xuecheng.framework.domain.course.ext.CategoryNode;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author Daniel Liu 2020/4/27 17:19
  */
+@Mapper
 public interface CategoryMapper {
     CategoryNode findList();
 }
