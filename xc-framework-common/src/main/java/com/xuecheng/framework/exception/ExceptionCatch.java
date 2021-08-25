a5961a8dc76683872618382912f9ab61d3c7c73f
liuqiang <liu1142865396@gmail.com>
2021-08-25 22:09:23 +0800

    update

--- xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
@@ -16,18 +16,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  * @author Daniel Liu 2019/11/27 10:06
  */
-@ControllerAdvice
+@ControllerAdvice //控制器增强
 public class ExceptionCatch {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionCatch.class);
 
 
     /**
-     * 使用EXCEPTIONS存放异常类型和错误代码的映射，ImmutableMap的特点的一旦创建不可改变，并且线程安全
+     * 使用 EXCEPTIONS 存放异常类型和错误代码的映射，ImmutableMap 的特点的一旦创建不可改变，并且线程安全
      */
-    public static ImmutableMap<Class<? extends Throwable>, ResultCode> EXCEPTIONS;
+    private static ImmutableMap<Class<? extends Throwable>, ResultCode> EXCEPTIONS;
 
     /**
-     * 使用builder来构建一个异常类型和错误代码的异常
+     * 使用 builder 来构建一个异常类型和错误代码的异常
      */
     protected static ImmutableMap.Builder<Class<? extends Throwable>, ResultCode> builder = ImmutableMap.builder();
 
@@ -36,13 +36,13 @@ public class ExceptionCatch {
     }
 
     /**
-     * 捕获CustomException可预知异常
+     * 捕获 CustomException 可预知异常
      *
      * @param e
      * @return
      */
     @ExceptionHandler( CustomException.class )
-    @ResponseBody //返回给前端json数据
+    @ResponseBody //返回给前端 json 数据
     private ResponseResult customException(CustomException e) {
         //记录日志
         LOGGER.error("catch exception:{}", e.getMessage(), e);
@@ -50,7 +50,7 @@ public class ExceptionCatch {
     }
 
     /**
-     * 捕获Exception不可以预知异常
+     * 捕获 Exception 不可以预知异常
      *
      * @param e
      * @return
