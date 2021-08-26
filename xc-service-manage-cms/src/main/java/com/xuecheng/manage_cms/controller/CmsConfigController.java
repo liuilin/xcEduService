5781d681e39032904eacc10683a0cf5278ec13ca
liuqiang <liu1142865396@gmail.com>
2021-08-26 22:16:00 +0800

    update

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsConfigController.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsConfigController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * CMS 内容管理模块
  * @author Daniel Liu 2019/12/2 10:11
  */
 
