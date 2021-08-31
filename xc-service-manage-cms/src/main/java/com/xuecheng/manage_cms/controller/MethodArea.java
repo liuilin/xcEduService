c0adcceed0d01fe7b71b6520d6f54ab13dcfa466
liuqiang <liu1142865396@gmail.com>
2021-08-31 17:48:12 +0800

    update

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/MethodArea.java
@@ -0,0 +1,15 @@
+package com.xuecheng.manage_cms.controller;
+
+/**
+ * @author liuqiang
+ * @since 2021-08-31
+ */
+public class MethodArea {
+    private String name = "Alice";
+
+    public static void main(String[] args) {
+        MethodArea m = new MethodArea();
+        m.name = "Lin";
+        m.name.equals("aa");
+    }
+}
